957f9578861408281e17450639434cf47b49d970
Franck Kirmann <franck.kirmann@gmail.com>
2023-12-13 05:53:56 +0100

    version 3 : version final

--- assets/script.js
+++ assets/script.js
@@ -18,5 +18,50 @@ const slides = [
   },
 ];
 
-var bannerImage = document.querySelector(".banner-img");
-bannerImage.src = "./assets/images/slideshow/slide2.jpg";
+let i = 0;
+const bannerImage = document.querySelector(".banner-img");
+const tagLine = document.querySelector("#banner p");
+const arrow_left = document.querySelector(".arrow_left");
+const displayslide = (index) => {
+  bannerImage.src = "./assets/images/slideshow/" + slides[index].image;
+  tagLine.innerHTML = slides[index].tagLine;
+  console.log(i);
+  for (let i = 0; i < dots.children.length; i++) {
+    const dot = dots.children[i];
+    if (i == index) {
+      dot.className = "dot dot_selected";
+    } else {
+      dot.className = "dot";
+    }
+  }
+};
+
+arrow_left.addEventListener("click", () => {
+  i--;
+  if (i === -1) {
+    i = slides.length - 1;
+  }
+  displayslide(i);
+});
+
+const arrow_right = document.querySelector(".arrow_right");
+arrow_right.addEventListener("click", () => {
+  i++;
+  if (i === slides.length) {
+    i = 0;
+  }
+  displayslide(i);
+});
+
+const dots = document.querySelector(".dots");
+for (let i = 0; i < slides.length; i++) {
+  const dot = document.createElement("button");
+  dot.className = "dot";
+  dots.appendChild(dot);
+  if (i === 0) {
+    dot.className = "dot dot_selected";
+  }
+  dot.addEventListener("click", () => {
+    displayslide(i);
+  });
+}
